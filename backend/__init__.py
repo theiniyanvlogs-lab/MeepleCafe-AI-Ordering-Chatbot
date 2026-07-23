85f623008db6e14a29b2d0cdf51ac6446bc590c7
theiniyanvlogs-lab <the.iniyan.vlogs@gmail.com>
2026-07-23 20:47:23 +0530

    Create __init__.py

---
+++ backend/__init__.py
@@ -0,0 +1,11 @@
+"""
+=========================================================
+Meeple Cafe AI Ordering Chatbot
+Backend Package Initialization
+Version : 3.0
+=========================================================
+"""
+
+__version__ = "3.0.0"
+__author__ = "Sugumar R"
+__project__ = "Meeple Cafe AI Ordering Chatbot"
